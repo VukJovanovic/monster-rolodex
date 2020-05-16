6825858a9b24273fab683bbb9828a8f3899aa523
Vuk Jovanovic <vukjovanovic34@gmail.com>
2020-05-16 22:06:35 +0200

    Updates

---
+++ precache-manifest.a1f631b8be41c1d4fd19086ebc13a79a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a597f1bb742dc0acceca63f90be509",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6530c6677ba3d3203e6a",
+    "url": "/monsters-rolodex/static/css/main.96238ce9.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6530c6677ba3d3203e6a",
+    "url": "/monsters-rolodex/static/js/main.caca5ecd.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
